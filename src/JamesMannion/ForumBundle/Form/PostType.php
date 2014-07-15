05903aae564f92def39d56edd0222a549f4d3aa5
James Mannion <jmannion@localhost.localdomain>
2014-07-15 21:55:49 +0100

    Added Post CRUD

---
+++ src/JamesMannion/ForumBundle/Form/PostType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace JamesMannion\ForumBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('body')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'JamesMannion\ForumBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'jamesmannion_forumbundle_post';
+    }
+}
